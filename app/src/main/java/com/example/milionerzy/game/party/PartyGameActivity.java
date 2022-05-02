21b05bbf66f54540bf4493906c338952ea3ea65b
Murasame <szymon.baranski012@gmail.com>
2022-05-02 15:44:11 +0200

    Views in GameFragment are set

--- app/src/main/java/com/example/milionerzy/game/party/PartyGameActivity.java
+++ app/src/main/java/com/example/milionerzy/game/party/PartyGameActivity.java
@@ -1,18 +1,21 @@
 package com.example.milionerzy.game.party;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.milionerzy.R;
-import com.example.milionerzy.model.Team;
 import com.example.milionerzy.services.TeamsListService;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 public class PartyGameActivity extends AppCompatActivity {
     private TeamsListService teamsListService;
+    private Button buttonA, buttonB,buttonC, buttonD, buttonNextQuestion;
+    private TextView textViewContentOfQuestion, textViewCurrentGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +23,12 @@ public class PartyGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_party_game);
 
         teamsListService = new TeamsListService(this);
-
+        findAllViews();
         setFragments();
 
     }
+
+
 
     private void setFragments() {
         TabLayout gameScoreTabLayout = findViewById(R.id.gameScoreTabLayout);
@@ -43,5 +48,14 @@ public class PartyGameActivity extends AppCompatActivity {
                 tab.setText("SCORE");
             }
         }).attach();
+    }
+    private void findAllViews() {
+        buttonA = findViewById(R.id.buttonAAnswerPartyMode);
+        buttonB = findViewById(R.id.buttonBAnswerPartyMode);
+        buttonC = findViewById(R.id.buttonCAnswerPartyMode);
+        buttonD = findViewById(R.id.buttonDAnswerPartyMode);
+        textViewContentOfQuestion = findViewById(R.id.textViewQuestionContentPartyMode);
+        textViewCurrentGroup = findViewById(R.id.textViewCurrentGroup);
+        buttonNextQuestion = findViewById(R.id.buttonNextQuestionPartyMode);
     }
 }
