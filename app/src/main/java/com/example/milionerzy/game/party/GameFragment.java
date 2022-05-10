060e49fc265ab56f8fee816ffa5c6acb40dae543
Murasame <szymon.baranski012@gmail.com>
2022-05-10 15:11:54 +0200

    After game finishes, user is redirected to mainActivity (fixed bug where all buttons were clickable on first question)

--- app/src/main/java/com/example/milionerzy/game/party/GameFragment.java
+++ app/src/main/java/com/example/milionerzy/game/party/GameFragment.java
@@ -2,6 +2,7 @@ package com.example.milionerzy.game.party;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import com.example.milionerzy.MainActivity;
 import com.example.milionerzy.R;
 import com.example.milionerzy.exceptions.PartyGameServiceException;
 import com.example.milionerzy.model.PartyGameDataToDisplay;
@@ -44,6 +46,7 @@ public class GameFragment extends Fragment {
         showQuestionContentAndAnswers();
         showCurrentTeam();
         showCurrentTeamScore();
+        setButtonsClickable(true);
         return mainView;
     }
 
@@ -161,9 +164,15 @@ public class GameFragment extends Fragment {
 
     private void showFinishGameAlertDialog() {
         new AlertDialog.Builder(getActivity())
-                .setTitle("Game is finished")
+                .setTitle("Game over")
                 .setCancelable(false)
-                .setPositiveButton("Ok", (dialog, id) -> dialog.dismiss())
+                .setPositiveButton("Finish Game", (dialog, id) -> {
+                    //TODO save game to database
+                    partyGameService.finishPartyGame();
+                    Intent intent = new Intent(getContext(), MainActivity.class);
+                    startActivity(intent);
+                    dialog.dismiss();
+                })
                 .create()
                 .show();
     }
